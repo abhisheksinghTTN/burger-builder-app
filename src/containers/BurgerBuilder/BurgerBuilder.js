89cbf482ebd9c962256eb2f49f82c3b18ad5ab6a
abhisheksinghTTN <abhishek.singh5@tothenew.com>
2021-04-05 22:55:37 +0530

    Forwarding unauthenticated users to authenticate and then order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,8 +37,14 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler = () => {
-        this.setState( { purchasing: true } );
-    }
+        if (this.props.isAuthenticated)
+        {
+            this.setState( { purchasing: true } );
+        }
+        else {
+            this.props.history.push('/auth');
+        }
+      }
 
     purchaseCancelHandler = () => {
         this.setState( { purchasing: false } );
@@ -69,6 +75,7 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}
                         price={this.props.price} />
                 </Aux>
             );
@@ -94,7 +101,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     };
 }
 
